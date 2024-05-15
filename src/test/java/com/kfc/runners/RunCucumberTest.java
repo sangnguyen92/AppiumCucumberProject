4a93385a4fa04e211319b176989a24ed632be987
sangnguyen92 <sangnguyen.k55@gmail.com>
2024-05-15 12:55:47 +0700

    Update RunCucumberTest.java

--- src/test/java/com/kfc/runners/RunCucumberTest.java
+++ src/test/java/com/kfc/runners/RunCucumberTest.java
@@ -1,45 +1,45 @@
-package com.kfc.runners;
+// package com.kfc.runners;
 
-import com.appium.utils.DriverManager;
-import com.appium.utils.GlobalParams;
-import com.appium.utils.ServerManager;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.apache.logging.log4j.ThreadContext;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.testng.annotations.DataProvider;
+// import com.appium.utils.DriverManager;
+// import com.appium.utils.GlobalParams;
+// import com.appium.utils.ServerManager;
+// import io.cucumber.testng.AbstractTestNGCucumberTests;
+// import org.apache.logging.log4j.ThreadContext;
+// import org.junit.AfterClass;
+// import org.junit.BeforeClass;
+// import org.testng.annotations.DataProvider;
 
-public class RunCucumberTest extends AbstractTestNGCucumberTests {
+// public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+//     @Override
+//     @DataProvider(parallel = true)
+//     public Object[][] scenarios() {
+//         return super.scenarios();
+//     }
 
-    @BeforeClass
-    public static void initialize() throws Exception {
-        GlobalParams params = new GlobalParams();
-        params.initializeGlobalParams();
+//     @BeforeClass
+//     public static void initialize() throws Exception {
+//         GlobalParams params = new GlobalParams();
+//         params.initializeGlobalParams();
 
-        ThreadContext.put("ROUTINGKEY", params.getPlatformName() + "_"
-                + params.getDeviceName());
+//         ThreadContext.put("ROUTINGKEY", params.getPlatformName() + "_"
+//                 + params.getDeviceName());
 
-        new ServerManager().startServer();
-        new DriverManager().initializeDriver();
-//        new DriverManager().initializeDriver2();
-    }
+//         new ServerManager().startServer();
+//         new DriverManager().initializeDriver();
+// //        new DriverManager().initializeDriver2();
+//     }
 
-    @AfterClass
-    public static void quit(){
-        DriverManager driverManager = new DriverManager();
-        if(driverManager.getDriver() != null){
-            driverManager.getDriver().quit();
-            driverManager.setDriver(null);
-        }
-        ServerManager serverManager = new ServerManager();
-        if(serverManager.getServer() != null){
-            serverManager.getServer().stop();
-        }
-    }
-}
+//     @AfterClass
+//     public static void quit(){
+//         DriverManager driverManager = new DriverManager();
+//         if(driverManager.getDriver() != null){
+//             driverManager.getDriver().quit();
+//             driverManager.setDriver(null);
+//         }
+//         ServerManager serverManager = new ServerManager();
+//         if(serverManager.getServer() != null){
+//             serverManager.getServer().stop();
+//         }
+//     }
+// }
